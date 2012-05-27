b1640056247ab5317a5c876a5ad5041f6829bb2d
Santosh Kumar <santosh79@gmail.com>
2012-05-27 15:44:04 -0700

    showing questions and answers correctly

--- bigNerdRanch/Quiz/Quiz/QuizViewController.m
+++ bigNerdRanch/Quiz/Quiz/QuizViewController.m
@@ -14,28 +14,24 @@
 
 @implementation QuizViewController
 
-- (void)viewDidLoad
-{
-    [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
-}
-
-- (void)viewDidUnload
-{
-    [super viewDidUnload];
-    // Release any retained subviews of the main view.
-}
-
-- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
-{
-    return (interfaceOrientation != UIInterfaceOrientationPortraitUpsideDown);
+- (id) initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil {
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        questions = [[NSMutableArray alloc] initWithObjects:@"What is 7 + 7?", @"What is the capital of Vermont?", @"From what is cognac made?", nil];
+        answers = [[NSMutableArray alloc] initWithObjects:@"14", @"Montpellier", @"Grapes", nil];
+        
+    }
+    return self;
 }
 
 - (IBAction)showQuestion:(id)sender {
-    questionField.text = @"Showing question";
+    currentQuestionsIndex = (currentQuestionsIndex + 1 >= [questions count]) ? 0 : (currentQuestionsIndex + 1);
+    NSString * question = [questions objectAtIndex:currentQuestionsIndex];
+    
+    questionField.text = question;
 }
 
 -(IBAction)showAnswer:(id)sender {
-    answerField.text = @"Showing answer";
+    answerField.text = [answers objectAtIndex:currentQuestionsIndex];
 }
 @end
