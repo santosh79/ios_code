7f4afbd90296e3dab518283a34ad05031c6852d2
Santosh Kumar <santosh79@gmail.com>
2012-05-27 15:04:24 -0700

    Added bigNerdRanch project

---
+++ bigNerdRanch/Quiz/Quiz/QuizAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  QuizAppDelegate.h
+//  Quiz
+//
+//  Created by SANTOSH KUMAR on 5/20/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class QuizViewController;
+
+@interface QuizAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) QuizViewController *viewController;
+
+@end
---
+++ bigNerdRanch/Quiz/Quiz/QuizViewController.h
@@ -0,0 +1,13 @@
+//
+//  QuizViewController.h
+//  Quiz
+//
+//  Created by SANTOSH KUMAR on 5/20/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface QuizViewController : UIViewController
+
+@end
