e6adcb8f288e8023ceb869d086f73bd8e1f9d567
Santosh Kumar <santosh79@gmail.com>
2012-05-31 18:09:29 -0700

    Added the Whereami project introduced in Chapter 4

---
+++ bigNerdRanch/Whereami/Whereami/WhereamiViewController.h
@@ -0,0 +1,16 @@
+//
+//  WhereamiViewController.h
+//  Whereami
+//
+//  Created by SANTOSH KUMAR on 5/31/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreLocation/CoreLocation.h>
+
+@interface WhereamiViewController : UIViewController {
+    CLLocationManager *locationManager;
+}
+
+@end
---
+++ bigNerdRanch/Whereami/Whereami/WhereamiViewController.m
@@ -0,0 +1,34 @@
+//
+//  WhereamiViewController.m
+//  Whereami
+//
+//  Created by SANTOSH KUMAR on 5/31/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "WhereamiViewController.h"
+
+@interface WhereamiViewController ()
+
+@end
+
+@implementation WhereamiViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation != UIInterfaceOrientationPortraitUpsideDown);
+}
+
+@end
