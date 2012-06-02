e3f5c0cbf76bbaf10afd72c90a2cb01068e8be85
Santosh Kumar <santosh79@gmail.com>
2012-06-02 14:03:53 -0700

    Added Hyponsister project required as part of Chapter 6

---
+++ bigNerdRanch/Hypnosister/Hypnosister/HypnosisView.m
@@ -0,0 +1,13 @@
+//
+//  HypnosisView.m
+//  Hypnosister
+//
+//  Created by SANTOSH KUMAR on 6/2/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "HypnosisView.h"
+
+@implementation HypnosisView
+
+@end
