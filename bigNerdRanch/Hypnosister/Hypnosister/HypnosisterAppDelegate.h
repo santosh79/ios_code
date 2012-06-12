eeca7cecd3908aa34540578ba3d9018904005244
Santosh Kumar <santosh79@gmail.com>
2012-06-12 13:29:42 -0700

    wip

---
+++ bigNerdRanch/Hypnosister/Hypnosister/HypnosisterAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  HypnosisterAppDelegate.h
+//  Hypnosister
+//
+//  Created by SANTOSH KUMAR on 6/12/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HypnosisterAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
